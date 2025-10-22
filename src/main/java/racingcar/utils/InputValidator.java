eb6c69d76ee2d1f501706d8034ab10de1d4016c0
seoyoon lee <leeseoyoon01@gmail.com>
2025-10-22 19:14:21 +0900

    feat: 자동차 이름 길이 검증 기능 구현

--- src/main/java/racingcar/utils/InputValidator.java
+++ src/main/java/racingcar/utils/InputValidator.java
@@ -8,6 +8,7 @@ public class InputValidator {
     public static List<String> validateCarNames(String CarNamesStr){
         List<String> carNames = convertToList(CarNamesStr);
         validateIsEmpty(carNames);
+        validateNameLength(carNames);
         return carNames;
     }
 
@@ -21,6 +22,14 @@ public class InputValidator {
         for (String str : list){
             if (str.isEmpty()){
                 throw new IllegalArgumentException("[ERROR] 빈 문자열은 이름이 될 수 없습니다.");
+            }
+        }
+    }
+
+    private static void validateNameLength(List<String> list){
+        for (String str : list){
+            if (str.length() > 5){
+                throw new IllegalArgumentException("[ERROR] 자동차 이름은 5자 이하여야 합니다.");
             }
         }
     }
